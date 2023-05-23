61f2dc1b5411ac7565883633beca73353e71d63a
Yassine Ait Laaguidi <aitlaaguidiyassine@gmail.com>
2023-05-23 19:48:00 +0200

    goal input component

--- App.js
+++ App.js
@@ -3,22 +3,15 @@ import { useState } from 'react';
 import {
     StyleSheet,
     View,
-    Button,
-    TextInput,
     FlatList,
 } from 'react-native';
 import GoalItem from './components/GoalItem';
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
     const [courseGoals, setCourseGoals] = useState([]);
 
-
-    function goalInputHandler(enteredText) {
-        setEnteredGoalText(enteredText);
-        console.log(enteredText)
-    }
-    function addGoalHandler() {
+    function addGoalHandler(enteredGoalText) {
        //console.log(enteredGoalText);
         setCourseGoals((currentCourseGoals) => [
             ...currentCourseGoals,
@@ -29,14 +22,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput
-              style={styles.textInput}
-              placeholder='Your course goal!'
-              onChangeText={goalInputHandler}
-          />
-          <Button title='Add goal' onPress={addGoalHandler}/>
-        </View>
+        <GoalInput onAddGoal={addGoalHandler} />
         <View style={styles.goalsContainer}>
             <FlatList
                 data={courseGoals}
@@ -57,26 +43,10 @@ const styles = StyleSheet.create({
   appContainer: {
     paddingTop:50,
     paddingHorizontal: 16,
-      flex: 1
+      flex: 1,
   },
-    inputContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
-        padding: 8
-    },
     goalsContainer: {
-        flex: 5
+        flex: 5,
     }
 })
 
